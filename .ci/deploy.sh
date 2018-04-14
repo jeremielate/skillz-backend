53dc0276b2e19515123312d2655a2af2bba5547a
Jérémie Lawson <jl@qrs.fr>
2018-04-15 00:07:11 +0200

    Mod.

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -8,5 +8,5 @@ commit=$2
 DATE=`date +%Y-%m-%d`
 
 for exe in `find ${dir} -type f`; do
-	curl --ftp-create-dirs -T ${dir}/${exe} ftp://skillz:YoloTest1234@skillz.events/${DATE}-${commit}/${exe}
+	curl --ftp-create-dirs -T ${exe} ftp://skillz:YoloTest1234@skillz.events/${DATE}-${commit}/`basename ${exe}`
 done
